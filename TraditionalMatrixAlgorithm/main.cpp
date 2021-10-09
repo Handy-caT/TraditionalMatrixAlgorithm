b0108b40e90dc63f17a645acc7e76f0df45655bf
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2021-10-09 19:33:53 +0300

    Initial Commit

---
+++ TraditionalMatrixAlgorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  TraditionalMatrixAlgorithm
+//
+//  Created by Максим on 9.10.21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
